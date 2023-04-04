ffa7e15088c1c9567d11c53e529bfc60eeaea442
̂̂̂̂̂̂̂̀̀̀̀̀RamonPloP <aspirelive02@gmail.com>
2023-04-04 11:42:06 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,5 @@
+let personas = ["Bob", "Esponja", "Patricio", "Perlita", "Don", "Cangrejo", "Arenita", "Plankton", "Papa pitufo", "Vanidoso", "Gloton", "Pitufina", "Saul", "Pedro", "Water", "Jessie", "Sam", "Pablito", "Tyron", "Uniqua"];
+
+for(let i=0; i<personas.length; i++) {
+    console.log(personas[i]);
+}
